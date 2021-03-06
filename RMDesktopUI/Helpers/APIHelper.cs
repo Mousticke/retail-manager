321fbb3d80a1c1d0be0ea32d39f5059477dae5a7
Akim Benchiha <akim.benchiha@gmail.com>
2021-03-06 17:40:16 +0100

    Wire up Login Form to API
    Connects the login form button to the Auth API endpoint (/token). gets back the Bearer token or the exception if failed

---
+++ RMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,55 @@
+﻿using RMDesktopUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        public HttpClient _apiClient;
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+
+            _apiClient = new HttpClient();
+            _apiClient.BaseAddress = new Uri(api);
+            _apiClient.DefaultRequestHeaders.Accept.Clear();
+            _apiClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AuthenticatedUser> AuthenticateAsync(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+            });
+
+            using (HttpResponseMessage response = await _apiClient.PostAsync("/token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
